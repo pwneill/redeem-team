0d0b65f251ed206e7e5623f32772898903fd9ea0
Alan Wagner <alanwagner101@gmail.com>
2019-02-23 11:24:40 -0600

    basic MERN application built

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Launch from "./pages/launch";
+import News from "./pages/news";
+import createEvent from "./pages/createEvent";
+import Events from "./pages/viewEvents";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import "./App.css"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Launch} />
+          <Route exact path="/news" component={News} />
+          <Route exact path="/create_event" component={createEvent} />
+          <Route exact path="/events" component={Events} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
