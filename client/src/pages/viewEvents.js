858a24f11656a4f144c8ca19482f5b57eaae800d
Andrew Burns <andrewburns@Andrews-MacBook-Air.local>
2019-02-24 10:25:12 -0600

    started creating the find events page

--- client/src/pages/viewEvents.js
+++ client/src/pages/viewEvents.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
 // import DeleteBtn from "../components/DeleteBtn";
 // import Jumbotron from "../components/Jumbotron";
 // import API from "../utils/API";
 // import { Link } from "react-router-dom";
-// import { Col, Row, Container } from "../components/Grid";
+import { Col, Row, Container } from "../components/Grid";
 // import { List, ListItem } from "../components/List";
 // import { Input, FormBtn } from "../components/Form";
 // import { Card, CardHeader, CardBody } from "../components/Card";
@@ -11,7 +12,16 @@ import React, { Component } from "react";
 class Events extends Component {
     render() {
         return (
-            <h1>Events Page</h1>
+            <Container fluid>
+              <Jumbotron id={"eventsJumbotron"}>
+              <h1>App Name</h1>
+              <h3>Events in your Area</h3>
+              </Jumbotron>
+              <Row fluid>
+              <Col size="md-9" offset="md-3">
+              </Col>
+              </Row>
+              </Container>
         )
     }
 }
