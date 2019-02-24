3e72b9f75ec8da41eee7ce4c6c7b5a1e77545a2e
Pwneill <phelan.oneill@gmail.com>
2019-02-24 16:20:53 -0600

    work on the submit - saving for andrew to see

--- client/src/pages/createEvent.js
+++ client/src/pages/createEvent.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
 import { Input, FormBtn } from "../components/Form";
 import { Col, Row, Container } from "../components/Grid";
-import fieldNames from "./newEvent.json";
+import fieldNames from "../utils/newEvent.json";
 import Jumbotron from "../components/Jumbotron";
 
 class createEvent extends Component {
   state = {
-    fieldNames
+    fieldNames,
+    inputvalue: "",
+    Name: "",
+    AddressLine1: "",
+    AddressLine2: "",
+    City: "",
+    State: "",
+    Game: "",
+    Console: "",
+    Description: "",
+    ImgSrc: "",
+    Q1: "",
+    Q2: "",
+    Limit: ""
   };
+
+  // constructor() {
+  //   super();
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+        [name]: value
+    });
+    console.log(this.state.Name)
+
+};
+
 
   render() {
     return (
@@ -18,12 +46,14 @@ class createEvent extends Component {
         </Jumbotron>
         <Row fluid>
           <Col size="md-9" offset="md-3">
-            <form className="mb-5">
+            <form className="mb-5" onSubmit={this.handleSubmit}>
               {this.state.fieldNames.map(question => (
                 <Input
                   key={question.id}
                   id={question.id}
                   name={question.name}
+                  value={question.name}
+                  onChange={this.handleInputChange}
                 />
               ))}
               <FormBtn>Submit</FormBtn>
