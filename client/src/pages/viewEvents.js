4430357bf8decdd68187f0d537ccdc8381d9b723
Andrew Burns <andrewburns@Andrews-MacBook-Air.local>
2019-02-28 19:37:51 -0600

    Card Lists are functioning properly

--- client/src/pages/viewEvents.js
+++ client/src/pages/viewEvents.js
@@ -8,6 +8,7 @@ import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
 // import { Input, FormBtn } from "../components/Form";
 import { Card, CardHeader, CardBody } from "../components/Card";
+import Button from "react-bootstrap/Button";
 
 class Events extends Component {
     state = {
@@ -35,40 +36,54 @@ class Events extends Component {
 
                     <Col size="md-10" offset="md-1">
                         {this.state.events.length ? (
-                            <Card id="maineventsCard" className="mt-2">
-                                {this.state.events.map(event => (
-                                    <CardBody key={event._id}>
-                                        <Row>
-                                            <Col size="md-3">
-                                                <img src={event.ImgSrc} width="300" length="300" />
-                                            </Col>
-                                            <Col size="md-6">
-                                                <div className="text-center">
-                                                    <strong>
-                                                        {event.Name}<br />
-                                                        on {event.Date}<br />
-                                                        {event.AddressLine1}<br />
-                                                        {event.AddressLine2}<br />
-                                                        {event.City}<br />
-                                                        {event.State}<br />
-                                                        {event.Zip}<br />
-                                                        {event.Game}<br />
-                                                        {event.Console}<br />
-                                                    </strong>
-                                                </div>
-                                            </Col>
-                                            <Col size="md-3">map</Col>
-                                        </Row>
-                                        <Row fluid>
-                                            <Col>
-                                                foo
+                            <List id={"overflow"}>
+                                <Card id="maineventsCard">
+                                    {this.state.events.map(event => (
+                                        <ListItem key={event._id} className="mt-2">
+                                            <CardBody>
+                                                <Row>
+                                                    <Col size="md-3">
+                                                        <img src={event.ImgSrc} width="300" length="300" />
+                                                    </Col>
+                                                    <Col size="md-6">
+                                                        <div className="text-center">
+                                                            <strong>
+                                                                {event.Name}<br />
+                                                                on {event.Date}<br />
+                                                                {event.AddressLine1}<br />
+                                                                {event.AddressLine2}<br />
+                                                                {event.City}<br />
+                                                                {event.State}<br />
+                                                                {event.Zip}<br />
+                                                                {event.Game}<br />
+                                                                {event.Console}<br />
+                                                            </strong>
+                                                        </div>
+                                                        <br />
+                                                    </Col>
+                                                    <Col size="md-3">{event.mapSrc}</Col>
+                                                    
+                                                </Row>
+                                                <Row fluid>
+                                                    <Col>
+                                                    <div className="text-center">
                                         {event.Description}
-                                            </Col>
-                                        </Row>
-                                    </CardBody>
+                                        </div>
+                                        <br />
+                                                    </Col>
+                                                    <Col>
+                                                        <Button href={`events/${event._id}`} className="float-left btn btn-dark">See More Details</Button>
+                                                        <Button href={`events/${event._id}/register`} className="float-right btn btn-dark">Register Here</Button>
+                                                    </Col>
+                                                </Row>
+                                        
+                                            </CardBody>
+                                            
 
-                                ))}
-                            </Card>
+                                        </ListItem>))}
+                                </Card>
+
+                            </List>
                         ) : (
                                 <Card>
                                     <h3 id="eventsbanner">No Results to Display</h3>
