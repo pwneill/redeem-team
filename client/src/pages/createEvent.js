e714a8f4bb5d2f17b6e49f906c79fc572a0b9719
Alan Wagner <alanwagner101@gmail.com>
2019-03-01 23:34:04 -0600

    changed around some jsx and styled all the main pages

--- client/src/pages/createEvent.js
+++ client/src/pages/createEvent.js
@@ -1,41 +1,78 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../components/Grid";
-// import fieldNames from "../utils/newEvent.json";
-import Jumbotron from "../components/Jumbotron";
-// import Form from "react-jsonschema-form";
+import fieldNames from "../utils/newEvent.json";
+import { Card, CardHeader, CardBody } from "../components/Card";
+import Form from "react-jsonschema-form";
 import API from "../utils/API"
+import { List, ListItem } from "../components/List";
 
-// const schema = fieldNames;
+const schema = fieldNames;
 
 class createEvent extends Component {
-  log = type => console.log.bind(console, type);
+    log = type => console.log.bind(console, type);
 
-  onSubmit = ({formData}, e) => {
-    API.saveEvent(formData);
-    console.log("Data submitted: ",  formData)
-  };
+    onSubmit = ({ formData }, e) => {
+        API.saveEvent(formData);
+        console.log("Data submitted: ", formData)
+    };
 
-  render() {
-    return (
-      <Container fluid>
-        <Jumbotron id={"eventsJumbotron"}>
-          <h1><strong>Gamers United</strong></h1>
-          <h3 id="eventsbanner">Add a New Event</h3>
-        </Jumbotron>
-        <div className="mb-5">
-        <Row fluid>
-          <Col size="md-6" offset="md-3">
-            {/* <Form
-              safeRenderCompletion={true}
-              schema={schema}
-              onSubmit={this.onSubmit}
-            /> */}
-          </Col>
-        </Row>
-        </div>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col size={"md-12"}>
+                        <Card id={"createHeaderCard"}>
+                            <Container fluid>
+                                <Row>
+                                    <Col size={"md-12"}>
+                                        <Card id={"createWordsCard"}>
+                                            <CardHeader>
+                                                <h1><strong>Gamers United</strong></h1>
+                                                <h3 id="eventsbanner">Add a New Event</h3>
+                                            </CardHeader>
+                                        </Card>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </Card>
+                        <Container fluid>
+                            <Row>
+                                <Col size={"md-12"}>
+                                    <Card id={"createMainContainer"}>
+                                        <CardBody>
+                                            <Col size={"md-12"}>
+                                                <List id={"createOverflow"}>
+                                                    <ListItem key={"1"}>
+                                                        <Card id={"createEventsCard"}>
+                                                            <CardBody>
+                                                                {<div className="mb-5">
+                                                                    <Row fluid>
+                                                                        <Col size="md-6" offset="md-3">
+                                                                            <Form
+                                                                                safeRenderCompletion={true}
+                                                                                schema={schema}
+                                                                                onSubmit={this.onSubmit}
+                                                                            />
+                                                                        </Col>
+                                                                    </Row>
+                                                                </div>}
+                                                            </CardBody>
+                                                        </Card>
+                                                    </ListItem>
+                                                </List>
+                                            </Col>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default createEvent;
+
+
--- client/src/pages/newUser.js
+++ client/src/pages/newUser.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+// import DeleteBtn from "../components/DeleteBtn";
+// import API from "../utils/API";
+// import { Link } from "react-router-dom";
+import { List, ListItem } from "../components/List";
+import { Col, Row, Container } from "../components/Grid";
+// import { Input, FormBtn } from "../components/Form";
+import { Card, CardBody } from "../components/Card";
 
 
 class newUser extends Component {
@@ -10,103 +17,89 @@ class newUser extends Component {
         }
 
         const regularInputWidth = {
-            "width": "450px"
+            "width": "100%"
         }
 
         const wideInputWidth = {
-            "width": "975px"
-        }
-
-        const headerStyle = {
-            "color": "#a9a9a9"
+            "width": "100%"
         }
 
         return (
-            <div>
-                <h1>New User</h1>
-                <br />
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12 center-block text-center">
-                            <h2 style={headerStyle} className="text-center">Create New User</h2>
-                        </div>
-                    </div>
-                    <br />
-
-                    <div className="row">
-                        <div style={inputStyle} ClassName="col-md-6">
-                            <input style={regularInputWidth} type="text" aria-label="First Name" className="form-control" placeholder="First Name"></input>
-                        </div>
-                        <div style={inputStyle} ClassName="col-md-6">
-                            <input style={regularInputWidth} type="text" aria-label="Last Name" className="form-control" placeholder="Last Name"></input>
-                        </div>
-
-                    </div>
-
-                    <div className="row">
-                        <div style={inputStyle} ClassName="col-md-12">
-                            <input style={wideInputWidth} type="text" aria-label="Email Address" className="form-control" placeholder="Email Address"></input>
-                        </div>
-
-
-                    </div>
-
-                    <div className="row">
-                        <div style={inputStyle} ClassName="col-md-12">
-                            <input style={wideInputWidth} type="text" aria-label="Phone Number" className="form-control" placeholder="Phone Number"></input>
-                        </div>
-
-                    </div>
-
-                    <div className="row">
-                        <div style={inputStyle} ClassName="col-md-6">
-                            <input style={regularInputWidth} type="text" aria-label="Preferred Gaming System" className="form-control" placeholder="Preferred Gaming System"></input>
-                        </div>
-                        <div style={inputStyle} ClassName="col-md-6">
-                            <input style={regularInputWidth} type="text" aria-label="Favorite Game" className="form-control" placeholder="Favorite Game"></input>
-                        </div>
-
-                    </div>
-
-                    <div className="row">
-                        <div style={inputStyle} ClassName="col-md-12">
-                            <input style={wideInputWidth} type="text" aria-label="Profile Picture" className="form-control" placeholder="Profile Picture - Paste a URL of your Photo"></input>
-                        </div>
-
-                    </div>
-
-                    <div className="row">
-                        <div style={inputStyle} ClassName="col-md-6">
-                            <input style={regularInputWidth} type="text" aria-label="Enter Password" className="form-control" placeholder="Enter Password"></input>
-                        </div>
-                        <div style={inputStyle} ClassName="col-md-6">
-                            <input style={regularInputWidth} type="text" aria-label="Re-Enter Password" className="form-control" placeholder="Re-Enter Password"></input>
-                        </div>
-
-                    </div>
-
-                    <br />
-                    <br />
-
-                    <div className="row">
-                        <div className="col-md-4 center-block">
-
-                        </div>
-
-                        <div className="col-md-4 center-block">
-                            <button type="button" class="btn btn-secondary">Create New User</button>
-                        </div>
-
-                        <div className="col-md-4 center-block">
-                        </div>
-                    </div>
-
-                </div>
-
-                <br />
-                <br />
-
-            </div>
+            <Container fluid>
+                <Row>
+                    <Col size={"md-12"}>
+                        <Card id={"userMainContainer"}>
+                            <CardBody>
+                                <Col size={"md-12"}>
+                                    <List id={"userOverflow"}>
+                                        <ListItem key={"1"}>
+                                            <Card id={"userEventsCard"}>
+                                                <CardBody>
+                                                    <Container fluid>
+                                                        <Row>
+                                                            <Col size={"md-12"} className="center-block text-center">
+                                                                <h2 className="text-center">Create New User</h2>
+                                                            </Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col size={"md-6"}>
+                                                                <input style={regularInputWidth} type="text" aria-label="First Name" className="form-control" placeholder="First Name"></input>
+                                                            </Col>
+                                                            <Col size={"md-6"}>
+                                                                <input style={regularInputWidth} type="text" aria-label="Last Name" className="form-control" placeholder="Last Name"></input>
+                                                            </Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col size={"md-12"} style={inputStyle}>
+                                                                <input style={wideInputWidth} type="text" aria-label="Email Address" className="form-control" placeholder="Email Address"></input>
+                                                            </Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col size={"md-12"} style={inputStyle}>
+                                                                <input style={wideInputWidth} type="text" aria-label="Phone Number" className="form-control" placeholder="Phone Number"></input>
+                                                            </Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col size={"md-6"}>
+                                                                <input style={regularInputWidth} type="text" aria-label="Preferred Gaming System" className="form-control" placeholder="Preferred Gaming System"></input>
+                                                            </Col>
+                                                            <Col size={"md-6"}>
+                                                                <input style={regularInputWidth} type="text" aria-label="Favorite Game" className="form-control" placeholder="Favorite Game"></input>
+                                                            </Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col size={"md-12"} style={inputStyle}>
+                                                                <input style={wideInputWidth} type="text" aria-label="Profile Picture" className="form-control" placeholder="Profile Picture - Paste a URL of your Photo"></input>
+                                                            </Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col size={"md-6"}>
+                                                                <input style={regularInputWidth} type="text" aria-label="Enter Password" className="form-control" placeholder="Enter Password"></input>
+                                                            </Col>
+                                                            <Col size={"md-6"}>
+                                                                <input style={regularInputWidth} type="text" aria-label="Re-Enter Password" className="form-control" placeholder="Re-Enter Password"></input>
+                                                            </Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col size={"md-4"} className="center-block">
+                                                            </Col>
+                                                            <Col size={"md-4"} className="center-block">
+                                                                <button type="button" id={"userButton"} className="btn btn-dark">Create New User</button>
+                                                            </Col>
+                                                            <Col size={"md-4"} className="center-block">
+                                                            </Col>
+                                                        </Row>
+                                                    </Container>
+                                                </CardBody>
+                                            </Card>
+                                        </ListItem>
+                                    </List>
+                                </Col>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
