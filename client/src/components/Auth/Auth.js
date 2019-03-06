e84089b185b63d8cf702cec9f3f7819e92489943
Andrew Burns <andrewburns@Andrews-MacBook-Air.local>
2019-03-05 20:29:23 -0600

    Updated Auth0

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -3,11 +3,11 @@ import auth0 from "auth0-js";
 
 
 class Auth {
-    constructor(){
+    constructor() {
         this.auth0 = new auth0.WebAuth({
             domain: 'rich-donovan.auth0.com',
             clientID: 'LHI8LEPW14lgTw6syHhIXfMhxMPPpRGU',
-            redirectUri:  'http://localhost:3000/' || 'https://powerful-beyond-98279.herokuapp.com/',
+            redirectUri: 'http://localhost:3000/' || 'https://powerful-beyond-98279.herokuapp.com/',
             audience: 'https://rich-donovan.auth0.com/userinfo',
             responseType: 'token id_token',
             scope: 'openid profile'
@@ -19,45 +19,63 @@ class Auth {
         this.signIn = this.signIn.bind(this);
         this.signOut = this.signOut.bind(this);
     }
-    getProfile(){
+    getProfile() {
         return this.profile;
     }
 
-    getIdToken(){
+    getIdToken() {
         return this.idToken;
     }
 
-    isAuthenticated(){
+    isAuthenticated() {
         let thing = new Date().getTime();
         let response = (thing < this.expiresAt);
         console.log("I am authenticated: ", response);
         console.log(this.expiresAt);
         return response;
     }
-    signIn(){
+    signIn() {
         this.auth0.authorize();
     }
-    
-    handleAuthentication(){
-        return new Promise((resolve,reject)=>{
-            this.auth0.parseHash((err,authResult)=> {
+
+    handleAuthentication() {
+        return new Promise((resolve, reject) => {
+            this.auth0.parseHash((err, authResult) => {
                 if (err) return reject(err);
-                if (!authResult || !authResult.idToken){
+                if (!authResult || !authResult.idToken) {
                     return reject(err);
                 }
-            this.idToken= authResult.idToken;
-            this.profile = authResult.idTokenPayload;
-            this.expiresAt = authResult.idTokenPayload.exp * 1000;
-            resolve();
+                this.setSession(authResult);
+                resolve();
             });
         })
     }
 
-    signOut(){
-        this.idToken = null;
-        this.profile= null;
-        this.expiresAt = null;
+    setSession(authResult) {
+        this.idToken = authResult.idToken;
+        this.profile = authResult.idTokenPayload;
+        this.expiresAt = authResult.idTokenPayload.exp * 1000;
     }
+
+    signOut() {
+        this.auth0.logout({
+            returnTo: 'http://localhost:3000',
+            clientID: 'LHI8LEPW14lgTw6syHhIXfMhxMPPpRGU',
+        });
+    }
+
+    silentAuth() {
+        return new Promise((resolve, reject) => {
+          this.auth0.checkSession({}, (err, authResult) => {
+            if (err) return reject(err);
+            this.setSession(authResult);
+            resolve();
+          });
+        });
+      }
+        
+    
+    
 }
 
 const auth0Client = new Auth();
